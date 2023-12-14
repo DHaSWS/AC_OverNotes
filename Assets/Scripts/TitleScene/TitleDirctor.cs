4540701b3453dc00c4ecc2e2738f5b2a975f5250
tyunimin <65494802+tyunimin@users.noreply.github.com>
2023-12-14 13:42:07 +0900

    title sound

--- Assets/Scripts/TitleScene/TitleDirctor.cs
+++ Assets/Scripts/TitleScene/TitleDirctor.cs
@@ -15,12 +15,13 @@ public class TitleDirctor : MonoBehaviour
 	private float feedColor = 1.0f;
 	private int select = 0;
 
+    [SerializeField] private AudioSource audio;
+
 	// Start is called before the first frame update
 	void Start()
 	{
 		//フェード画像のフェードアウト
 		ONFade.SetFadeOut(this, 0.5f, fadeImage, () => { });
-		
 	}
    
 	// Update is called once per frame
@@ -35,7 +36,7 @@ public class TitleDirctor : MonoBehaviour
 			return;
 		}
 
-		var spaceKey = current[Key.Space];
+		var fKey = current[Key.F];
 		var ESCKey = current[Key.Escape];
         var upKey = current[Key.UpArrow];
         var downKey = current[Key.DownArrow];
@@ -52,24 +53,76 @@ public class TitleDirctor : MonoBehaviour
 
         Math.Clamp(select, 0, 1);
 
-		//スペースキーが押された場合フェードインし曲選択に移行
-        if (current.fKey.wasPressedThisFrame)
-			ONFade.SetFadeIn(this, 0.5f, fadeImage, () => { SceneManager.LoadScene("Scenes/SelectScene"); });
-		else if(ESCKey.wasPressedThisFrame )
-		{
-			#if UNITY_EDITOR
-				UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
-			#else
+        //スペースキーが押された場合フェードインし曲選択に移行
+        if (fKey.wasPressedThisFrame)
+            ONFade.SetFadeIn(this, 0.5f, fadeImage, () => { SceneManager.LoadScene("Scenes/SelectScene"); });
+        else if (ESCKey.wasPressedThisFrame)
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
+#else
 			    Application.Quit();//ゲームプレイ終了
-			#endif
+#endif
         }
-
 
         if (pushSpaceKey.color.a >= 1 || pushSpaceKey.color.a <= 0)
 			feedColor *= -1;
 
-
 		pushSpaceKey.color = new Color(pushSpaceKey.color.r, pushSpaceKey.color.g, pushSpaceKey.color.b, pushSpaceKey.color.a + (feedColor * Time.deltaTime));
-		
-	}
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //シーン遷移の入力を自由にさせる為に作りました。消す時は//で囲んだところを消してください。　
+        audio.volume += 0.01f;
+        Awake();
+        OnDestroy();
+        OnEnable();
+        OnDisable();
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////
+    }
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private InputAction _anyKeyAction;
+
+    private void Awake()
+    {
+        // 任意キーのAction作成
+        _anyKeyAction = new InputAction(
+            "AnyKey",
+            InputActionType.Button,
+            "<Keyboard>/anyKey"
+        );
+
+        // コールバック登録
+        _anyKeyAction.performed += OnAnyKey;
+    }
+
+    private void OnDestroy()
+    {
+        // コールバック解除
+        _anyKeyAction.performed -= OnAnyKey;
+
+        // Actionの破棄
+        _anyKeyAction.Dispose();
+    }
+
+    private void OnEnable()
+    {
+        // Actionの有効化
+        _anyKeyAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        // Actionの無効化
+        _anyKeyAction.Disable();
+    }
+
+    // 任意キーが押されたときの処理
+    private void OnAnyKey(InputAction.CallbackContext context)
+    {
+        ONFade.SetFadeIn(this, 0.5f, fadeImage, () => { SceneManager.LoadScene("Scenes/SelectScene"); });
+        
+    }
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
