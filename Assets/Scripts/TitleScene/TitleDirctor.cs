76e777ab30dcd3a4377440fcb5159a136609f3aa
tyunimin <65494802+tyunimin@users.noreply.github.com>
2023-12-21 12:16:30 +0900

    TitleScene_Keybind

--- Assets/Scripts/TitleScene/TitleDirctor.cs
+++ Assets/Scripts/TitleScene/TitleDirctor.cs
@@ -7,25 +7,24 @@ using UnityEngine.Experimental.Rendering;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
+using OverNotes;
 public class TitleDirctor : MonoBehaviour
 {
 	[SerializeField] private Image fadeImage;
 	[SerializeField] private SpriteRenderer pushAnyKey;
 	private float feedColor = 1.0f;
 
-    [SerializeField] private AudioSource bgm_audio = new AudioSource();
-    private bool bgm = false;
+    [SerializeField] private AudioSource bgm_audio;
 
-    private InputAction _anyKeyAction;
-
-	// Start is called before the first frame update
-	void Start()
+    // Start is called before the first frame update
+    void Start()
 	{
 		//フェード画像のフェードアウト
 		ONFade.SetFadeOut(this, 0.5f, fadeImage, () => { });
         bgm_audio.volume = 0.0f;
-	}
+
+        KeyManager.Instance.StartRebinding();
+    }
    
 	// Update is called once per frame
 	void Update()
@@ -37,13 +36,9 @@ public class TitleDirctor : MonoBehaviour
 		if (current == null)
 		{
 			return;
-		}
+        }
 
-		var ESCKey = current[Key.Escape];
-
-        //スペースキーが押された場合フェードインし曲選択に移行
-        if (bgm == true)
-            bgm_audio.volume -= 0.02f;
+        var ESCKey = current[Key.Escape];
 
         if (ESCKey.wasPressedThisFrame)
         {
@@ -57,63 +52,25 @@ public class TitleDirctor : MonoBehaviour
         if (pushAnyKey.color.a >= 1 || pushAnyKey.color.a <= 0)
 			feedColor *= -1;
 
-		pushAnyKey.color = new Color(pushAnyKey.color.r, pushAnyKey.color.g, pushAnyKey.color.b, pushAnyKey.color.a + (feedColor * Time.deltaTime));
-
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //シーン遷移の入力を自由にさせる為に作りました。消す時は//で囲んだところを消してください。　
         bgm_audio.volume += 0.01f;
-        
-        Awake();
-        OnDestroy();
-        OnEnable();
-        OnDisable();
-        
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////
+        if(bgm_audio.volume>=0.4f)
+        {
+            bgm_audio.volume = 0.4f;
+        }
+
+        pushAnyKey.color = new Color(pushAnyKey.color.r, pushAnyKey.color.g, pushAnyKey.color.b, pushAnyKey.color.a + (feedColor * Time.deltaTime));
+       
+        for(int i=0;i<4;i++)
+        {
+            if (KeyManager.Instance.IsFinishedRebind[i] == true)
+            {
+                if (KeyManager.Instance.IsFinishedRebind[3]==true)
+                {
+                    bgm_audio.volume -= 0.001f;
+                    if (SceneManager.GetActiveScene().name == "TitleScene")
+                        ONFade.SetFadeIn(this, 0.5f, fadeImage, () => { SceneManager.LoadScene("Scenes/SelectScene"); });
+                }
+            }
+        }
     }
-
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    private void Awake()
-    {
-        // 任意キーのAction作成
-        _anyKeyAction = new InputAction(
-            "AnyKey",
-            InputActionType.Button,
-            "<Keyboard>/anyKey"
-        );
-
-        // コールバック登録
-            _anyKeyAction.performed += OnAnyKey;
-    }
-
-    private void OnDestroy()
-    {
-        // コールバック解除
-            _anyKeyAction.performed -= OnAnyKey;
-
-        // Actionの破棄
-        _anyKeyAction.Dispose();
-    }
-
-    private void OnEnable()
-    {
-        // Actionの有効化
-        _anyKeyAction.Enable();
-    }
-
-    private void OnDisable()
-    {
-        // Actionの無効化
-        _anyKeyAction.Disable();
-    }
-
-    // 任意キーが押されたときの処理
-    private void OnAnyKey(InputAction.CallbackContext context)
-    {
-        bgm = true;
-        if (SceneManager.GetActiveScene().name == "TitleScene")
-            ONFade.SetFadeIn(this, 0.5f, fadeImage, () => { SceneManager.LoadScene("Scenes/SelectScene"); });
-    }
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
