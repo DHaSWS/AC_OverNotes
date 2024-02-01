7860853fd55cb4cac1cd42531788a210acb7c37c
momijinoki <1106niumaleta@gmail.com>
2024-02-01 16:40:41 +0900

    longNoteEffect追加

--- Assets/Scripts/PlayScene/Player/PlayerController.cs
+++ Assets/Scripts/PlayScene/Player/PlayerController.cs
@@ -11,13 +11,15 @@ public class PlayerController : MonoBehaviour {
     [SerializeField] AudioSource se;
     [SerializeField] private InputActionAsset asset;
     [SerializeField] InputActionTrace trace;
-    [SerializeField] EffekseerEffectAsset effect;
-
+    [SerializeField] EffekseerEffectAsset tapEffect;
+    [SerializeField] EffekseerEffectAsset longEffect;
+    EffekseerHandle longEffectHandle;
     // Invoke actions
     static public event Action<int> OnPressLane;
 
     //エフェクトの拡大率
-    private static Vector3 m_effectScale = new Vector3(0.7f, 0.7f, 0.7f);
+    private static Vector3 m_tapEffectScale = new Vector3(0.7f, 0.7f, 0.7f);
+    private static Vector3 m_longEffectScale = new Vector3(0.3f, 0.3f, 0.3f);
 
 
     private void Awake() {
@@ -80,7 +82,15 @@ public class PlayerController : MonoBehaviour {
         // Play SE
         se.PlayOneShot(se.clip);
 
-        PlayTapEffect(index);
+        if(note.tag=="Long")
+        {
+            PlayLongEffect(index);
+        }
+        else
+        {
+            PlayTapEffect(index);
+        }
+        
 
         // Judge
         noteController.JudgeNormal(Mathf.Abs((float)(triggeredTime - noteController.param.beatTime)));
@@ -98,6 +108,13 @@ public class PlayerController : MonoBehaviour {
         GameObject note = PlayData.Lanes[index].GetChild(0).gameObject;
         NoteController noteController = note.GetComponent<NoteController>();
         noteController.JudgeHold(Mathf.Abs((float)(triggeredTime - noteController.param.beatEndTime)));
+
+        if (note.tag == "Long")
+        {
+            StopLongEffect();
+        }
+        
+
     }
 
     private void OnDestroy() {
@@ -117,14 +134,35 @@ public class PlayerController : MonoBehaviour {
         //エフェクトの座標計算
         Vector3 effectPosition = new Vector3(-1.5f + ((pushNumber) * 1.0f), -5, 0.5f);
 
+        //エフェクト
+        EffekseerHandle handle = EffekseerSystem.PlayEffect(tapEffect, effectPosition);
+        //拡大率
+        handle.SetScale(m_tapEffectScale);
+
+        
+        Debug.Log("playEffect!!");
+    }
+
+    private void PlayLongEffect(int pushNumber)
+    {
+        //エフェクトの座標計算
+        Vector3 effectPosition = new Vector3(-1.5f + ((pushNumber) * 1.0f), -5, 0.5f);
+        Quaternion q = this.gameObject.transform.rotation;
+        Quaternion effectRotation = new Quaternion(90, q.y+90, 90, q.w);
 
         //エフェクト
-        EffekseerHandle handle = EffekseerSystem.PlayEffect(effect, effectPosition);
-        //拡大率
-        handle.SetScale(m_effectScale);
-        //角度
-        //handle.SetRotation(effectRotation);
+        longEffectHandle = EffekseerSystem.PlayEffect(longEffect, effectPosition);
 
-        Debug.Log("playEffect!!");
+        //拡大率
+        longEffectHandle.SetScale(m_longEffectScale);
+        //角度
+        longEffectHandle.SetRotation(effectRotation);
+        
+        Debug.Log("playLongEffect!!");
+    }
+
+    private void StopLongEffect()
+    {
+        longEffectHandle.Stop();
     }
 }
